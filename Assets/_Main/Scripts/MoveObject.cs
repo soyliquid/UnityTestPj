995e3f214349a69fb18185fdbbdab77124161f28
soyliquid <soyliquid@gmail.com>
2014-06-15 15:35:22 +0900

    first

---
+++ Assets/_Main/Scripts/MoveObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveObject : MonoBehaviour {
+
+	public Vector3 MoveVector;
+	public Vector3 RotateVector;
+	public float Speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(MoveVector * Time.deltaTime * Speed);
+		transform.Rotate(RotateVector * Time.deltaTime * Speed);
+	}
+}
